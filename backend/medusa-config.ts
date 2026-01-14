06f393ed6f5d496cce96f03e2e62b06355c1d4bb
Brian <bsdacres@gmail.com>
2026-01-13 19:41:58 -0500

    updated cors

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
+      storeCors: "https://mikdraumida-d9jf.vercel.app",
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
@@ -42,3 +42,4 @@ module.exports = defineConfig({
     },
   ],
 })
+
