064af0306b377481660749bb82586ef70086c7cd
Brian <bsdacres@gmail.com>
2026-01-13 20:07:01 -0500

    Attempt to fit cors

--- client/src/lib/medusa.ts
+++ client/src/lib/medusa.ts
@@ -7,5 +7,5 @@ const key ="pk_be7897e58bd0d0b903d8f87ac97327b95beb51dfc2970ac382c0b437f3073a43"
 export const sdk = new Medusa({
     baseUrl: MEDUSA_BACKEND_URL,
     debug: true,
-    publishableKey: key,
+    publishableKey: "pk_be7897e58bd0d0b903d8f87ac97327b95beb51dfc2970ac382c0b437f3073a43",
 })
