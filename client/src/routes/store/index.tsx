d8de43259641c7a953b7d8844fa8892a60b6be4c
Brian Dacres <88354099+bsdacres@users.noreply.github.com>
2026-01-31 11:20:52 -0500

    Integrate Facebook Pixel for tracking events
    Added Facebook Pixel tracking script to store page.

--- client/src/routes/store/index.tsx
+++ client/src/routes/store/index.tsx
@@ -17,6 +17,21 @@ export default function StorePage() {
   return (
     <main class="store-page">
       <Title>Store | Mik Draumida</Title>
+          <script
+              innerHTML={`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '1462735488401118');
+                fbq('track', 'PageView');
+                fbq('track', 'AddToCart');
+              `}
+            />
       
       <div class="store-header">
         <h1 class="store-title">All Products</h1>
